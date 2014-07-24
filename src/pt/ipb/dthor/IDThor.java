a48889031ec5c4cfbaf02925c8ab099846f3cb55
Esfomeado <vps1992@live.com.pt>
2014-07-24 21:15:12 +0100

    Fix

--- src/pt/ipb/dthor/IDThor.java
+++ src/pt/ipb/dthor/IDThor.java
@@ -1,11 +1,12 @@
 package pt.ipb.dthor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import pt.ipb.dthor.torrent.DThorTorrent;
 
 public interface IDThor {
 
-    public void addTorrent(DThorTorrent torrent);
+    public void addTorrent(DThorTorrent torrent) throws IOException;
 
-    public ArrayList<DThorTorrent> searchTorrent(String name);
+    public ArrayList<DThorTorrent> searchTorrent(String name) throws ClassNotFoundException, IOException;
 }
